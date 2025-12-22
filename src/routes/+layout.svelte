0acc06381db5c084254d530aaf1a74e4b2776fb3
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2025-12-22 14:54:09 -0500

    fill out svelte:head

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,37 @@
 </script>
 
 <svelte:head>
+    <title>Thessa</title>
     <link rel="icon" href={favicon} />
+    <meta name="title" content="Thessa" />
+    <meta
+        name="description"
+        content="An AI-powered thesaurus that intelligently generates synonyms."
+    />
+    <meta name="author" content="Ethan" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Thessa" />
+    <meta
+        property="og:description"
+        content="An AI-powered thesaurus that intelligently generates synonyms."
+    />
+    <meta property="og:site_name" content="Thessa" />
+    <meta property="twitter:card" content="summary" />
+    <meta property="twitter:title" content="Thessa" />
+    <meta
+        property="twitter:description"
+        content="An AI-powered thesaurus that intelligently generates synonyms."
+    />
+    <link rel="canonical" href="https://ethmarks.github.io/thessa" />
+
+    <link
+        rel="stylesheet"
+        href="https://ethmarks.github.io/css/global.min.css"
+    />
+    <script
+        src="https://ethmarks.github.io/js/ethmarks-components.js"
+        defer
+    ></script>
 </svelte:head>
 
 {@render children()}
