a22ce74292850d23137957f8b5a442051a2cb533
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2025-12-22 15:27:42 -0500

    add basic main and h1 styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,38 @@
 <ethmarks-header active="projects"></ethmarks-header>
 
 <main>
-    <h1>Thessa</h1>
+    <div id="heading">
+        <h1 class="nomint">Thessa</h1>
+    </div>
 </main>
 
 <ethmarks-footer source="https://github.com/ethmarks/thessa"></ethmarks-footer>
+
+<style>
+    main {
+        margin-inline: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        min-height: calc(100vh - var(--main-margin-top) - 45px);
+        overflow-x: hidden;
+        width: 100%;
+        padding: 0;
+    }
+
+    #heading {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 12px;
+        margin-bottom: 10px;
+
+        h1 {
+            color: var(--color-accent-light);
+            font-weight: 700;
+            font-size: 3.2rem;
+            letter-spacing: 0.2px;
+        }
+    }
+</style>
