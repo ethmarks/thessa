5d3f2009aa5fef2ce14bd124dfacfdf1c5e5656b
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2025-12-22 16:15:46 -0500

    improve QueryBar styles

--- src/lib/components/query-bar.svelte
+++ src/lib/components/query-bar.svelte
@@ -3,16 +3,19 @@
 <style>
     input {
         margin: 0 auto 10rem;
-        padding: 1rem;
+        padding: 1rem 1.5rem;
         width: 100%;
         max-width: 800px;
 
-        border-radius: var(--radius-lg);
+        border-radius: 999px;
         border: var(--border-accent-t);
         outline: none;
         background: rgba(255, 255, 255, 0.03);
 
+        text-align: center;
         color: var(--color-text);
-        font-size: var(--font-size-lg);
+        font-size: var(--font-size-h4);
+        font-family: var(--font-secondary);
+        caret-color: var(--color-accent);
     }
 </style>
