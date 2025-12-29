213555e2f9529cdc0a37565881d9db18da17dd10
Ethan Marks <87040432+ColourlessSpearmint@users.noreply.github.com>
2025-12-29 11:59:35 -0500

    add styles to synonym-list.svelte and synonym.svelte

--- src/lib/components/synonym-list.svelte
+++ src/lib/components/synonym-list.svelte
@@ -16,3 +16,15 @@
         <Synonym text={synonymText}></Synonym>
     {/each}
 </ul>
+
+<style>
+    ul {
+        display: flex;
+        flex-wrap: wrap;
+        gap: var(--spacing-sm);
+        width: 100%;
+        list-style: none;
+        padding: 0;
+        margin: 0 auto 10rem;
+    }
+</style>
--- src/lib/components/synonym.svelte
+++ src/lib/components/synonym.svelte
@@ -2,4 +2,37 @@
     let { text } = $props();
 </script>
 
-<li>{text}</li>
+<li>
+    <button>{text}</button>
+</li>
+
+<style>
+    li {
+        display: contents;
+    }
+
+    button {
+        padding: var(--spacing-xs) var(--spacing-sm);
+        background-color: rgba(255, 255, 255, 0.03);
+        border: var(--border-accent-t);
+        color: var(--color-accent-light);
+        border-radius: var(--radius-md);
+        cursor: pointer;
+        transition: all 0.2s ease;
+        user-select: none;
+        font-family: inherit;
+        font-size: inherit;
+
+        &:hover {
+            background-color: rgba(255, 255, 255, 0.05);
+            border-color: var(--border-accent);
+            transform: translateY(-2px);
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }
+
+        &:active {
+            transform: translateY(0);
+            box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+        }
+    }
+</style>
